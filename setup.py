d4010c2d1289b8133f178a3dbd12c3fc649c1459
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-17 21:16:42 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from  setuptools  import setup
+setup(name='patterns',
+version='0.1',
+description='Patterns of capital Alphabets, Small Alphabets, Numbers and Symbools',
+url=https://github.com/Ashokkommi0001/patterns.git,
+author='Kommi Ashok Naidu',
+author_email='ashokkommi3@gmail.com',
+license='MIT',
+packages=['patterns'],
+zip_safe=False)
