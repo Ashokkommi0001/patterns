2d2d1875afbdaddec7ea018f5a16997edfe63aca
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-17 16:50:59 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,13 @@
+from Alp.cap_alp import A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+
+
+from Alp.small_alp import a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
+
+
+
+from Num import _1,_2,_3,_4,_5,_6,_7,_8,_9
+
+
+
+
+from Symbols import heart, hexagon, left_num_tri, left_num_tri_1, left_num_tri_2, left_reverse_triangle, left_triangle, num_tree, num_tree_1, parlalogram, pentagon, rectangle, reverse_left_num_tri, reverse_triangle, right_num_tri, right_reverse_triangle, right_triangle, squear, triangle
