4ae62d3c819aedd93e4f34aca4ca3ae94c1be430
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:39:27 +0530

    Update Y.py

--- Alp/cap_alp/Y.py
+++ Alp/cap_alp/Y.py
@@ -1,8 +1,21 @@
-def Y():
+def for_Y():
     for row in range(7):
         for col in range(7):
             if (row-col==0 and row<3) or (row+col==6 and row<3) or (row>2 and col==3):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_Y():
+    i=0
+    while i<7:
+        j=0
+        while j<7:
+            if (i-j==0 and i<3) or (i+j==6 and i<3) or (i>2 and j==3):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            j+=1
+        i+=1
         print()
