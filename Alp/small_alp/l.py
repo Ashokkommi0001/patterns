d2653cb224a15fa542e4bdcbbcce79887b088cc6
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:53:10 +0530

    Update l.py

--- Alp/small_alp/l.py
+++ Alp/small_alp/l.py
@@ -1,8 +1,21 @@
-def l():
+def for_l():
     for row in range(7):
         for col in range(5):
             if (col==0) or (row==6 and col<3):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_l():
+    row=0
+    while row<7:
+        col=0
+        while col<5:
+            if (col==0) or (row==6 and col<3):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
