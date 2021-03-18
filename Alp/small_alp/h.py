2810e498169b55208b17e196d2796bfc7dddd696
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:49:55 +0530

    Update h.py

--- Alp/small_alp/h.py
+++ Alp/small_alp/h.py
@@ -1,8 +1,21 @@
-def h():
+def for_h():
     for row in range(7):
         for col in range(6):
             if (col==0 ) or ((row==3) and col<5) or (col==5 and (row>3)):
                 print("*", end=" ")
             else:
                 print(" ", end=" ")
+        print()
+
+def while_h():
+    row=0
+    while row<7:
+        col=0
+        while col<6:
+            if (col==0 ) or ((row==3) and col<5) or (col==5 and (row>3)):
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+            col+=1
+        row+=1
         print()
