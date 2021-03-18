b1cbc9442c4b0530c3fbfddad32934a94c2e06de
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:29:12 +0530

    Update K.py

--- Alp/cap_alp/K.py
+++ Alp/cap_alp/K.py
@@ -1,8 +1,21 @@
-def K():
+def for_K():
     for row in range(7):
         for col in range(5):
             if (col==0) or (row+col==4 or row-col==2):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_K():
+    i=0
+    while i<7:
+        j=0
+        while j<5:
+            if (j==0) or (i+j==4 or i-j==2):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            j+=1
+        i+=1
         print()
