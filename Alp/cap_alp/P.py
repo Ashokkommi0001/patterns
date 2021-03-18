a6077fe87a5488cf3b734bbcb0409a772472c194
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:33:27 +0530

    Update P.py

--- Alp/cap_alp/P.py
+++ Alp/cap_alp/P.py
@@ -1,8 +1,21 @@
-def P():
+def for_P():
     for row in range(7):
         for col in range(6):
             if col==0 or (row%3==0 and col<5 and row<6) or (col==5 and row%3!=0 and row<3):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_P():
+    i=0
+    while i<7:
+        j=0
+        while j<6:
+            if j==0 or (i%3==0 and j<5 and i<6) or (j==5 and i%3!=0 and i<3):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            j+=1
+        i+=1
         print()
