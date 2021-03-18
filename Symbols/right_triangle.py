65ec9b78f75ceb48de739632c36f310b53dc619e
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:36:56 +0530

    Update right_triangle.py

--- Symbols/right_triangle.py
+++ Symbols/right_triangle.py
@@ -1,8 +1,21 @@
-def right_triangle():
+def for_right_triangle():
     for row in range(6):
         for col in range(6):
             if (col==5 or row==5) or row+col==5:
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_right_triangle():
+    row=0
+    while row<6:
+        col=0
+        while col<6:
+            if (col==5 or row==5) or row+col==5:
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
