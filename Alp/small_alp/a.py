372b7bbc11e147f027c764d41326c4e8ac954136
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:45:29 +0530

    Update a.py

--- Alp/small_alp/a.py
+++ Alp/small_alp/a.py
@@ -1,8 +1,22 @@
-def a():
+def for_a():
     for row in range(8):
         for col in range(7):
             if (row%3==0 and (col>0 and col<5)) or (col==5 and (row>0 and row<7)) or ((row==1 or row==4 or row==5) and col==0) or (row+col==13):
                 print("*", end=" ")
             else:
                 print(" ", end=" ")
+        print()
+
+def while_a():
+    row=0
+    while row<8:
+        col=0
+        while col<7:
+   
+            if (row%3==0 and (col>0 and col<5)) or (col==5 and (row>0 and row<7)) or ((row==1 or row==4 or row==5) and col==0) or (row+col==13):
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+            col+=1
+        row+=1
         print()
