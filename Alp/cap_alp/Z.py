52af72abd3ae297f430707526b50fee04e7e47a0
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:40:14 +0530

    Update Z.py

--- Alp/cap_alp/Z.py
+++ Alp/cap_alp/Z.py
@@ -1,8 +1,21 @@
-def Z():
+def for_Z():
     for row in range(7):
         for col in range(7):
             if row==0 or row==6 or row+col==6 :
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_Z():
+    i=0
+    while i<7:
+        j=0
+        while j<7:
+            if i==0 or i==6 or i+j==6 :
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            j+=1
+        i+=1
         print()
