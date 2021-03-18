fb303e8ab76dbf848351a009f5b467169b9e46bc
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:46:46 +0530

    Update c.py

--- Alp/small_alp/c.py
+++ Alp/small_alp/c.py
@@ -1,8 +1,21 @@
-def c():
+def for_c():
     for row in range(7):
         for col in range(5):
             if (col==0 and (row!=0 and row!=6))  or ((row==0 or row==6) and (col>0)):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+        
+def while_c():
+    row=0
+    while row<7:
+        col=0
+        while col<5:
+            if (col==0 and (row!=0 and row!=6))  or ((row==0 or row==6) and (col>0)):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
