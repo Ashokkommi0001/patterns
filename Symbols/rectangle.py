1cd7ef24c6a2f992db4b5ce7c44c4e6e4eb53262
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:34:15 +0530

    Update rectangle.py

--- Symbols/rectangle.py
+++ Symbols/rectangle.py
@@ -1,8 +1,21 @@
-def rectangle():
+def for_rectangle():
     for row in range(5):
         for col in range(10):
             if row==0 or row==4 or col==0 or col==9:
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_rectangle():
+    row=0
+    while row<5:
+        col=0
+        while col<10:
+            if row==0 or row==4 or col==0 or col==9:
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
