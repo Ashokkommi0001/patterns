c86125c64189f188f35e157e4f7fe9f83bb7f264
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:57:06 +0530

    Update r.py

--- Alp/small_alp/r.py
+++ Alp/small_alp/r.py
@@ -1,8 +1,21 @@
-def r():
+def for_r():
     for row in range(7):
         for col in range(6):
             if (col==2 and row>0) or (row<1 and col>2) or row==col==0:
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_r():
+    row=0
+    while row<7:
+        col=0
+        while col<6:
+            if (col==2 and row>0) or (row<1 and col>2) or row==col==0:
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
