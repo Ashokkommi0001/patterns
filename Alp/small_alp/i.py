a469002ca89840937e9a26dfcece93240b9f42b5
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:50:40 +0530

    Update i.py

--- Alp/small_alp/i.py
+++ Alp/small_alp/i.py
@@ -1,8 +1,21 @@
-def i():
+def for_i():
     for row in range(9):
         for col in range(7):
             if (col==3 or row==8) and row>1 or (row>2 and row+col==3) or row==0 and col==3 :
                 print("*",end="")
             else:
                 print(end=" ")
+        print()
+
+def while_i():
+    row=0
+    while row<9:
+        col=0
+        while col<7:
+            if (col==3 or row==8) and row>1 or (row>2 and row+col==3) or row==0 and col==3 :
+                print("*",end="")
+            else:
+                print(end=" ")
+            col+=1
+        row+=1
         print()
