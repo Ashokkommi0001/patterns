08c7e14e18c68394576316730ed815c3273cf701
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:58:52 +0530

    Update u.py

--- Alp/small_alp/u.py
+++ Alp/small_alp/u.py
@@ -1,8 +1,21 @@
-def u():
+def for_u():
     for row in range(7):
         for col in range(6):
             if ((col==0 or col==4) and row<5) or (row==5 and col>0 and col<4) or  (row+col==10) and row!=6:
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_u():
+    row=0
+    while row<7:
+        col=0
+        while col<6:
+            if ((col==0 or col==4) and row<5) or (row==5 and col>0 and col<4) or  (row+col==10) and row!=6:
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
