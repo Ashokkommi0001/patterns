98438f5d642c68a0f33048b399761fc44204b419
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:47:54 +0530

    Update e.py

--- Alp/small_alp/e.py
+++ Alp/small_alp/e.py
@@ -1,8 +1,21 @@
-def e():
+def for_e():
     for row in range(7):
         for col in range(5):
             if (col==0 and (row!=0 and row!=6))  or ((row%3==0)  and (col>0 and col<4)) or (col==4 and (row>0 and row<=2)):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_e():
+    row=0
+    while row<7:
+        col=0
+        while col<5:
+            if (col==0 and (row!=0 and row!=6))  or ((row%3==0)  and (col>0 and col<4)) or (col==4 and (row>0 and row<=2)):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
