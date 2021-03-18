b98d78ec2863ee0fe78899139bfc8e15cf5a6e95
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:59:53 +0530

    Update w.py

--- Alp/small_alp/w.py
+++ Alp/small_alp/w.py
@@ -1,8 +1,21 @@
-def w():
+def for_w():
     for row in range(6):
         for col in range(7):
             if (col%3==0 and row<5) or (row==5 and col%3!=0):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_w():
+    row=0
+    while row<6:
+        col=0
+        while col<7:
+            if (col%3==0 and row<5) or (row==5 and col%3!=0):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
