a798f89922097468370d9738426be19d9fe03296
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:04:27 +0530

    Update _1.py

--- Num/_1.py
+++ Num/_1.py
@@ -1,4 +1,4 @@
-def _1():
+def for_1():
     for row in range(7):
         for col in range(7):
             if (col==3 or row==6) or (row<3 and row+col==2):
@@ -7,3 +7,16 @@ def _1():
                 print(end=" ")
         print()
 
+
+def while_1():
+    row=0
+    while row<7:
+        col=0
+        while col<7:
+            if (col==3 or row==6) or (row<3 and row+col==2):
+                print("*",end="")
+            else:
+                print(end=" ")
+            col+=1
+        row+=1
+        print()
