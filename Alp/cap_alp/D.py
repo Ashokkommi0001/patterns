c7449f583069b0027cbec7231b2e5721aabab1e7
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:20:00 +0530

    Update D.py

--- Alp/cap_alp/D.py
+++ Alp/cap_alp/D.py
@@ -6,3 +6,16 @@ def D():
             else:
                 print(end="  ")
         print()
+
+def D():
+    i=0
+    while i<7:
+        j=0
+        while j<5:
+            if (j==0) or (j==4 and (i!=0 and i!=6)) or (i==0 or i==6)  and (j>0 and j<4):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            j+=1
+        i+=1
+        print()
