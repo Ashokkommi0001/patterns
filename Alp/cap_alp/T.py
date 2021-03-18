de1b7386fcdd904d75247e3f8bc718aa70bc5d6f
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:36:03 +0530

    Update T.py

--- Alp/cap_alp/T.py
+++ Alp/cap_alp/T.py
@@ -1,8 +1,21 @@
-def T():
+def for_T():
     for row in range(6):
         for col in range(5):
             if (row==0 or col==2):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_T():
+    i=0
+    while i<6:
+        j=0
+        while j<5:
+            if (i==0 or j==2):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            j+=1
+        i+=1
         print()
