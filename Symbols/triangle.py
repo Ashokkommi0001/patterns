c3e72b5e02ddc9d1809132266a78a270d0c58a0b
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:38:26 +0530

    Update triangle.py

--- Symbols/triangle.py
+++ Symbols/triangle.py
@@ -1,8 +1,21 @@
-def triangle():
+def for_triangle():
     for row in range(6):
         for col in range(11):
             if (row==5) or (col-row==5 or row+col==5):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_triangle():
+    row=0
+    while row<6:
+        col=0
+        while col<11:
+            if (row==5) or (col-row==5 or row+col==5):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
