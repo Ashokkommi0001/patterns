a322604cc93b4b3c422743cdf2e8722df313fb30
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:18:44 +0530

    Update B.py

--- Alp/cap_alp/B.py
+++ Alp/cap_alp/B.py
@@ -6,3 +6,16 @@ def B():
             else:
                 print(end="  ")
         print()
+
+def B():
+    i=0
+    while i<7:
+        j=0
+        while j<6:
+            if j==0 or (i%3==0 and j<5) or (j==5 and i%3!=0):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            j+=1
+        i+=1
+        print()
