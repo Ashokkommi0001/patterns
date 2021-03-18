cabd81b8a1e683b3f64f88ce7800e99388e489bc
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:08:42 +0530

    Update and rename _2.py to two.py

--- Num/two.py
+++ Num/two.py
@@ -1,4 +1,4 @@
-def for_2():
+def for_two():
     for row in range(5):
         for col in range(4):
             if (row==4 or row+col==4)  or (row==0 and col!=0) or (row==1 and col<1):
@@ -7,7 +7,7 @@ def for_2():
                 print(end="  ")
         print()
 
-def while_2():
+def while_two():
     row=0
     while row<5:
         col=0
