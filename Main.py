ce5eefaf5052577afdfd16f221444570b871774f
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:40:02 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from Alp.small_alp import a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
 
 
 
-from Num import _1,_2,_3,_4,_5,_6,_7,_8,_9
+from Num import zero, one, two, three, four, five, six, seven, eight, nine
 
 
 
