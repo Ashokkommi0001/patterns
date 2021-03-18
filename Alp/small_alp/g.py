221e636d67af81788ebcfb0ef0610c93441fe7ec
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:49:25 +0530

    Update g.py

--- Alp/small_alp/g.py
+++ Alp/small_alp/g.py
@@ -1,8 +1,21 @@
-def g():
+def for_g():
     for row in range(7):
         for col in range(5):
             if (row%3==0) and (col>0 and col<4) or (col==0 and (row%3!=0 and row!=4)) or (col==4 and row<6):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_g():
+    row=0
+    while row<7:
+        col=0
+        while col<5:
+            if (row%3==0) and (col>0 and col<4) or (col==0 and (row%3!=0 and row!=4)) or (col==4 and row<6):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
