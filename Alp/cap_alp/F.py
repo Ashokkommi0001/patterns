ff208277a29bb7b1e685bca3f09917a3e29f7c15
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:21:28 +0530

    Update F.py

--- Alp/cap_alp/F.py
+++ Alp/cap_alp/F.py
@@ -6,3 +6,16 @@ def F():
             else:
                 print(end="  ")
         print()
+
+def F():
+    i=0
+    while i<7:
+        j=0
+        while j<5:
+            if (j==0) or (i==0 or i==3):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            j+=1
+        i+=1
+        print()
