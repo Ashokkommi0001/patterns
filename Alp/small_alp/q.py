17785bb4bf1e40c3ff34d8f09da21176ef710fd8
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:56:27 +0530

    Update q.py

--- Alp/small_alp/q.py
+++ Alp/small_alp/q.py
@@ -1,8 +1,22 @@
-def q():
+def for_q():
     for row in range(7):
         for col in range(6):
             if col==4 or (col==0 and (row==1 or row==2)) or ((row==0 or row==3) and (col>0 and col<4)) or (row==5 and col==5):
                 print("*",end=" ")
             else:
                     print(end="  ")
+        print()
+
+def while_q():
+    row=0
+    while row<7:
+        col=0
+        while col<6:
+            if col==4 or (col==0 and (row==1 or row==2)) or ((row==0 or row==3) and (col>0 and col<4)) or (row==5 and col==5):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
+                
         print()
