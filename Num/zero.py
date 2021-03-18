9c71622528fa9e6a22276fb31e905e673a5ac941
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:07:38 +0530

    Update and rename _0.py to zero.py

--- Num/zero.py
+++ Num/zero.py
@@ -1,4 +1,4 @@
-def for_0():
+def for_zero():
     for row in range(7):
         for col in range(5):
             if ((col==0 or col==4) and (row!=0 and row!=6)) or (col>0 and col<4) and (row==0 or row==6) or (row+col==5):
@@ -7,7 +7,7 @@ def for_0():
                 print(end="  ")
         print()
 
-def while_0():
+def while_zero():
     row=0
     while row<7:
         col=0
