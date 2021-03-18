564996befafef1875473b988947ef2015e5404fd
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:59:21 +0530

    Update v.py

--- Alp/small_alp/v.py
+++ Alp/small_alp/v.py
@@ -1,8 +1,21 @@
-def v():
+def for_v():
     for row in range(8):
         for col in range(16):
             if (row-col==0 or row+col==14):
                 print("*",end="")
             else:
                 print(end=" ")
+        print()
+
+def while_v():
+    row=0
+    while row<8:
+        col=0
+        while col<16:
+            if (row-col==0 or row+col==14):
+                print("*",end="")
+            else:
+                print(end=" ")
+            col+=1
+        row+=1
         print()
