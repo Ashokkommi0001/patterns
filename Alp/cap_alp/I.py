fb5fc2c71e8cff56ac3006f4b84a0ddb5a3d9d98
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:27:24 +0530

    Update I.py

--- Alp/cap_alp/I.py
+++ Alp/cap_alp/I.py
@@ -1,8 +1,21 @@
-def I():
+def for_I():
     for row in range(7):
         for col in range(5):
             if (col==2) or (row==0 or row==6):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_I():
+    i=0
+    while i<7:
+        j=0
+        while j<5:
+            if (j==2) or (i==0 or i==6):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            j+=1
+        i+=1
         print()
