718b2fc69f1db09493a9fc5bb66f2d82971d2eb3
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:52:31 +0530

    Update k.py

--- Alp/small_alp/k.py
+++ Alp/small_alp/k.py
@@ -1,8 +1,21 @@
-def k():
+def for_k():
     for row in range(7):
         for col in range(5):
             if (col==0) or (row>1 and (row+col==5  or row-col==3)):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_k():
+    row=0
+    while row<7:
+        col=0
+        while col<5:
+            if (col==0) or (row>1 and (row+col==5  or row-col==3)):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
