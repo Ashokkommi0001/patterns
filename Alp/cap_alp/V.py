49029531bd1554ca2a9b09db269ef4e421eec0a3
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:37:22 +0530

    Update V.py

--- Alp/cap_alp/V.py
+++ Alp/cap_alp/V.py
@@ -1,8 +1,21 @@
-def V():
+def for_V():
     for row in range(8):
         for col in range(16):
             if (row-col==0 or row+col==14):
                 print("*",end="")
             else:
                 print(end=" ")
+        print()
+
+def while_V():
+    i=0
+    while i<8:
+        j=0
+        while j<16:
+            if (i-j==0 or i+j==14):
+                print("*",end="")
+            else:
+                print(end=" ")
+            j+=1
+        i+=1
         print()
