53e65f5a6dd45f3768e35b96a30bf2713b74ff2c
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:24:25 +0530

    Update D.py

--- Alp/cap_alp/D.py
+++ Alp/cap_alp/D.py
@@ -1,4 +1,4 @@
-def D():
+def for_D():
     for row in range(7):
         for col in range(5):
             if (col==0) or (col==4 and (row!=0 and row!=6)) or (row==0 or row==6)  and (col>0 and col<4):
@@ -7,7 +7,7 @@ def D():
                 print(end="  ")
         print()
 
-def D():
+def while_D():
     i=0
     while i<7:
         j=0
