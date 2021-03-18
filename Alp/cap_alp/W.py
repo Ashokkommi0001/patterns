96a488a599bf00069499eb526294c610db025313
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:38:01 +0530

    Update W.py

--- Alp/cap_alp/W.py
+++ Alp/cap_alp/W.py
@@ -1,8 +1,21 @@
-def W():
+def for_W():
     for row in range(6):
         for col in range(5):
             if (col==0 or col==4) or ( row>2 and (row+col==5 or row-col==1)):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_W():
+    i=0
+    while i<6:
+        j=0
+        while j<5:
+            if (j==0 or j==4) or (i>2 and (i+j==5 or i-j==1)):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            j+=1
+        i+=1
         print()
