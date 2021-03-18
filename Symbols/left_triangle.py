c8b51054bf5e4893731bbcfb998ff01809353f41
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:32:03 +0530

    Update left_triangle.py

--- Symbols/left_triangle.py
+++ Symbols/left_triangle.py
@@ -1,8 +1,21 @@
-def left_triangle():
+def for_left_triangle():
     for row in range(6):
         for col in range(6):
             if (col==0 or row==5) or row-col==0:
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_left_triangle():
+    row=0
+    while row<6:
+        col=0
+        while col<6:
+            if (col==0 or row==5) or row-col==0:
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
