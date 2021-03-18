b29398841d5dafb866af6cfc93e84e3cd6e06945
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:19:22 +0530

    Update C.py

--- Alp/cap_alp/C.py
+++ Alp/cap_alp/C.py
@@ -7,3 +7,16 @@ def C():
                 print(end="  ")
         print()
 
+
+def C():
+    i=0
+    while i<7:
+        j=0
+        while j<5:
+            if (j==0 and (i!=0 and i!=6))  or ((i==0 or i==6) and (j>0)):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            j+=1
+        i+=1
+        print()
