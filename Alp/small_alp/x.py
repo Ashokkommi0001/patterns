6e1928a967c231d648f444d0693b9fc1127ce7e2
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:00:24 +0530

    Update x.py

--- Alp/small_alp/x.py
+++ Alp/small_alp/x.py
@@ -1,8 +1,21 @@
-def x():
+def for_x():
     for row in range(5):
         for col in range(5):
             if (row-col==0 or row+col==4):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_x():
+    row=0
+    while row<5:
+        col=0
+        while col<5:
+            if (row-col==0 or row+col==4):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
