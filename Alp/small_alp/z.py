95a428809e9f1e2cb029756c33f8acbaaac854a4
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:01:46 +0530

    Update z.py

--- Alp/small_alp/z.py
+++ Alp/small_alp/z.py
@@ -1,8 +1,21 @@
-def z():
+def for_z():
     for row in range(5):
         for col in range(5):
             if row==0 or row==4 or row+col==4 :
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_z():
+    row=0
+    while row<5:
+        col=0
+        while col<5:
+            if row==0 or row==4 or row+col==4 :
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
