32ee545e2db85ff21a9b6003432ec803a7eed050
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:28:30 +0530

    Update J.py

--- Alp/cap_alp/J.py
+++ Alp/cap_alp/J.py
@@ -1,8 +1,21 @@
-def J():
+def for_J():
     for row in range(7):
         for col in range(5):
             if (col==2) or (row==0) or (row==6 and col<2):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_J():
+    i=0
+    while i<7:
+        j=0
+        while j<5:
+            if (j==2) or (i==0) or (i==6 and j<2):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            j+=1
+        i+=1
         print()
