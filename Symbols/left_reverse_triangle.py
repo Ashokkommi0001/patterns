ae2c07bf4984bb05804d7d59654abe26a75113c0
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:31:13 +0530

    Update left_reverse_triangle.py

--- Symbols/left_reverse_triangle.py
+++ Symbols/left_reverse_triangle.py
@@ -1,8 +1,21 @@
-def left_reverse_triangle():
+def for_left_reverse_triangle():
     for row in range(6):
         for col in range(6):
             if (col==0 or row==0) or row+col==5:
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_left_reverse_triangle():
+    row=0
+    while row<6:
+        col=0
+        while col<6:
+            if (col==0 or row==0) or row+col==5:
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
