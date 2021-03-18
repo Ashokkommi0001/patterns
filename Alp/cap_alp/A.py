7483cb169f842f9fe80c1833b9877264cea5b1a1
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:16:38 +0530

    Update A.py

--- Alp/cap_alp/A.py
+++ Alp/cap_alp/A.py
@@ -1,8 +1,21 @@
-def A():
+def for_A():
     for i in range(7):
         for j in range(5):
             if ((j==0 or j==4) and i!=0) or (j>0 and j<4) and (i==0 or i==3):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_A():
+    i=0
+    while i<7:
+        j=0
+        while j<5:
+            if ((j==0 or j==4) and i!=0) or (j>0 and j<4) and (i==0 or i==3):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            j+=1
+        i+=1 
         print()
