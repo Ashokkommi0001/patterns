8fa5e39ade66b54f0723a66e8a241095fd275b35
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:31:55 +0530

    Update N.py

--- Alp/cap_alp/N.py
+++ Alp/cap_alp/N.py
@@ -1,8 +1,21 @@
-def N():
+def for_N():
     for row in range(7):
         for col in range(7):
             if (col==0 or col==6) or row-col==0:
                 print("*",end="  ")
             else:
                 print(end="   ")
+        print()
+
+def while_N():
+    i=0
+    while i<7:
+        j=0
+        while j<7:
+            if (j==0 or j==6) or i-j==0:
+                print("*",end="  ")
+            else:
+                print(end="   ")
+            j+=1
+        i+=1        
         print()
