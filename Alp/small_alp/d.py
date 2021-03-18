2594d9148618041d382544aeed0610eda3f5dbbb
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:47:23 +0530

    Update d.py

--- Alp/small_alp/d.py
+++ Alp/small_alp/d.py
@@ -1,8 +1,21 @@
-def d():
+def for_d():
     for row in range(7):
         for col in range(6):
             if (col==5 ) or ((row==3 or row==6) and col>0) or (col==0 and (row>3 and row<6)):
                 print("*", end=" ")
             else:
                 print(" ", end=" ")
+        print()
+
+def while_d():
+    row=0
+    while row<7:
+        col=0
+        while col<6:
+            if (col==5 ) or ((row==3 or row==6) and col>0) or (col==0 and (row>3 and row<6)):
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+            col+=1
+        row+=1
         print()
