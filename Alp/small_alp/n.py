2ed1a8046563bbb516a0e8e4c737fc0fc2f6c018
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:54:35 +0530

    Update n.py

--- Alp/small_alp/n.py
+++ Alp/small_alp/n.py
@@ -1,8 +1,21 @@
-def n():
+def for_n():
     for row in range(5):
         for col in range(4):
             if row==col==0 or (col%3==0 and row>0) or (row==0 and col%3!=0):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_n():
+    row=0
+    while row<5:
+        col=0
+        while col<4:
+            if row==col==0 or (col%3==0 and row>0) or (row==0 and col%3!=0):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
