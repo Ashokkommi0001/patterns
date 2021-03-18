0ddcfa4e900606df7b4b515a9af80f8ec4674abb
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:37:40 +0530

    Update squear.py

--- Symbols/squear.py
+++ Symbols/squear.py
@@ -1,8 +1,21 @@
-def squear():
+def for_squear():
     for row in range(7):
         for col in range(7):
             if row==0 or row==6 or col==0 or col==6:
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_squear():
+    row=0
+    while row<7:
+        col=0
+        while col<7:
+            if row==0 or row==6 or col==0 or col==6:
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
