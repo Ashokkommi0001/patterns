453c24379ee14d33b970ded4ab1b95819adde808
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:38:40 +0530

    Update X.py

--- Alp/cap_alp/X.py
+++ Alp/cap_alp/X.py
@@ -1,8 +1,20 @@
-def X():
+def for_X():
     for row in range(7):
         for col in range(7):
             if (row-col==0 or row+col==6):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+def while_X():
+    i=0
+    while i<7:
+        j=0
+        while j<7:
+            if (i-j==0 or i+j==6):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            j+=1
+        i+=1
         print()
