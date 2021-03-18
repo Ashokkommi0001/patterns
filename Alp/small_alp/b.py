28574af0c5911b6527a7785c76b5e65a01a30bba
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 12:46:00 +0530

    Update b.py

--- Alp/small_alp/b.py
+++ Alp/small_alp/b.py
@@ -1,8 +1,21 @@
-def b():
+def for_b():
     for row in range(7):
         for col in range(6):
             if (col==0 ) or ((row==3 or row==6) and col<5) or (col==5 and (row>3 and row<6)):
                 print("*", end=" ")
             else:
                 print(" ", end=" ")
+        print()
+
+def while_b():
+    row=0
+    while row<7:
+        col=0
+        while col<6:
+            if (col==0 ) or ((row==3 or row==6) and col<5) or (col==5 and (row>3 and row<6)):
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+            col+=1
+        row+=1
         print()
