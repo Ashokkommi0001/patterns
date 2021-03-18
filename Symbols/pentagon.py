97c850bd69a6ea55340cb9a3c49d9caba9ee12d0
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:33:31 +0530

    Update pentagon.py

--- Symbols/pentagon.py
+++ Symbols/pentagon.py
@@ -1,8 +1,21 @@
-def pentagon():
+def for_pentagon():
     for row in range(10):
         for col in range(9):
             if (row==9) or (row>4 and (col==0 or col==8)) or (row+col==4 or col-row==4):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_pentagon():
+    row=0
+    while row<10:
+        col=0
+        while col<9:
+            if (row==9) or (row>4 and (col==0 or col==8)) or (row+col==4 or col-row==4):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
