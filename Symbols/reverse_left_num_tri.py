4bb258f99c6b341525f0bd91833d675bc1056c4e
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 17:24:31 +0530

    Update reverse_left_num_tri.py

--- Symbols/reverse_left_num_tri.py
+++ Symbols/reverse_left_num_tri.py
@@ -1,5 +1,15 @@
-def reverse_left_num_tri():
+def for_reverse_left_num_tri():
     for i in range(9, 0, -1):
         for j in range(i):
             print(i, end=" ")
+        print()
+
+def while_reverse_left_num_tri():
+    i=9
+    while i>=1:
+        j=1
+        while j<=i:
+            print(i, end=" ")
+            j+=1
+        i-=1
         print()
