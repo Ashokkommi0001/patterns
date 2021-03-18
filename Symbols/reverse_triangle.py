454b43c7a760500e9f4de7f23e90ffe6563819de
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:35:04 +0530

    Update reverse_triangle.py

--- Symbols/reverse_triangle.py
+++ Symbols/reverse_triangle.py
@@ -1,8 +1,21 @@
-def reverse_triangle():
+def for_reverse_triangle():
     for row in range(6):
         for col in range(11):
             if (row==0) or (row-col==0 or row+col==10):
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_reverse_triangle():
+    row=0
+    while row<6:
+        col=0
+        while col<11:
+            if (row==0) or (row-col==0 or row+col==10):
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
