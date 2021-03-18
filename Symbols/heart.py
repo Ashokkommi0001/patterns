64c29590ae621c76523a5c171f7139942cdc12a3
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 13:26:50 +0530

    Update heart.py

--- Symbols/heart.py
+++ Symbols/heart.py
@@ -1,8 +1,21 @@
-def heart():
+def for_heart():
     for row in range(6):
         for col in range(7):
             if row==0 and col%3!=0 or row==1 and col%3==0 or row-col==2 or row+col==8:
                 print("*",end=" ")
             else:
                 print(end="  ")
+        print()
+
+def while_heart():
+    row=0
+    while row<6:
+        col=0
+        while col<7:
+            if row==0 and col%3!=0 or row==1 and col%3==0 or row-col==2 or row+col==8:
+                print("*",end=" ")
+            else:
+                print(end="  ")
+            col+=1
+        row+=1
         print()
