44bb847542f0a969d1b7496b1237c348f9d03db1
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-18 16:31:20 +0530

    Update left_num_tri.py

--- Symbols/left_num_tri.py
+++ Symbols/left_num_tri.py
@@ -1,5 +1,16 @@
-def left_num_tri():
+def for_left_num_tri():
     for i in range(10):
         for j in range(i):
             print(i, end=" ")
         print()
+
+def while_left_num_tri():
+    row=1
+    while row<=8:
+        col=1
+        while col<=row:
+            print(row, end=" ")
+            col+=1
+        row+=1
+        print()
+left_num_tri()
